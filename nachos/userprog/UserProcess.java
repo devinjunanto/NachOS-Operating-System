79b129264e1cb095f23813cad2057c10008812ea
sssaiya <sssaiya@ucsd.edu>
2019-11-11 16:00:56 -0800

    debug

--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -551,8 +551,8 @@ public class UserProcess {
 		OpenFile openFile = files[fileDescriptor];
 		if (openFile == null)
 			return -1;
-		//System.out.println("Successfully opened file " + fileDescriptor + " to WRITE");
-		//System.out.println("Attempting to WRITE " + count + " bytes");
+		System.out.println("Successfully opened file " + fileDescriptor + " to WRITE");
+		System.out.println("Attempting to WRITE " + count + " bytes \n");
 
 		// bytesLeftToWrite = count;
 		// currentPos = pointer;
