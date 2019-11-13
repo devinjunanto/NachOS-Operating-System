b81a1253dd5e067fdd24860be582826e4888d707
sssaiya <sssaiya@ucsd.edu>
2019-11-13 12:02:20 -0800

    Debug

--- nachos/test/close.c
+++ nachos/test/close.c
@@ -6,7 +6,6 @@
 #include "syscall.h"
 #include "stdlib.h"
 
-// reading or writing to a file descriptor that was closed returns an error.
 int main(int argc, char *argv[])
 {
     int val = 0;
@@ -28,7 +27,7 @@ int close1()
         printf("OKAY - Unable to close file(-1 returned)\n");
     else
         printf("BAD - -1 not returned\n");
-    
+
     assert(val == -1);
 
     return 0;
@@ -45,14 +44,33 @@ int close2()
     int val = creat(name);
     printf("The file descriptor is - %d\n", val);
 
-    printf("Closing File test.txt - %d\n",val);
+    printf("Closing File test.txt - %d\n", val);
     int closeVal = close(val);
     assert(closeVal == 0);
 
     int newVal = open(name);
-    printf("Opening File test.txt again\n");
+    printf("Opening/Creating File test2.txt \n");
     printf("The file descriptor is - %d\n", newVal);
     //assert(hasFile(val) == false);
-    //assert(val == newVal);
+    assert(val == newVal);
+    return 0;
+}
+
+// reading or writing to a file descriptor that was closed returns an error.
+int close3()
+{
+    printf("\nRunning Close3\n\n");
+    printf("ReCreating File test.txt\n");
+    char *name = "test.txt";
+    int fileDesc = creat(name);
+    char *str = "Roses are Red\nI am Ded\n";
+    int numWritten = write(fileDesc, str, 30);
+    assert(numWritten == 22);
+
+    int closed = close(fileDesc);
+    assert(closed == 0);
+
+    int numWrittenAgain = write(fileDesc, str, 30);
+    assert(numWrittenAgain == -1);
     return 0;
 }
