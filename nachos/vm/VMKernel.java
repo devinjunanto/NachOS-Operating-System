e562e3de8f550da39d37e9af2254eed88e650ede
devinjunanto <44035919+devinjunanto@users.noreply.github.com>
2019-12-05 18:37:35 -0800

    Add files via upload

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -1,11 +1,11 @@
 package nachos.vm;
 
-import java.util.ArrayList;
-
 import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * A kernel that can support multiple demand-paging user processes.
@@ -16,6 +16,15 @@ public class VMKernel extends UserKernel {
 	 */
 	public VMKernel() {
 		super();
+		pageSize = Machine.processor().getNumPhysPages();
+		int i = 0;
+		while(i < pageSize)
+		{
+			clkCtr.add(new Integer[]{-1.-1});
+			processes.add(null);
+			i++;
+		}
+		clkIdx = 0;
 	}
 
 	/**
@@ -23,6 +32,7 @@ public class VMKernel extends UserKernel {
 	 */
 	public void initialize(String[] args) {
 		super.initialize(args);
+		swp = ThreadedKernel.fileSystem.open("swap", true);
 	}
 
 	/**
@@ -43,32 +53,97 @@ public class VMKernel extends UserKernel {
 	 * Terminate this kernel. Never returns.
 	 */
 	public void terminate() {
+		swp.close();
+		ThreadedKernel.fileSystem.remove("swap");
 		super.terminate();
 	}
+
+	public static int pageReplacement(int vpn, VMProcess process){
+		int clock = clkCtr.get(clkIdx)[1];
+		while(clock > 0)
+		{
+			clkCtr.get(clkIdx)[1] = 0;
+			clkIdx++;
+			if(clkIdx == pageSize) clkIdx = 0;
+			clock = clkCtr.get(clkIdx < 1];
+		}
+		int a = clkIdx;
+		VMProcess vProcess = processes.get(a);
+		vProcess.unloadSection(clkCtr.get(a)[0].intValue());
+		clkCtr.set(a, new Integer[]{vpn, 1});
+		pArray.set(a, process);
+		return a;
+	}
+
+	public static void printTable(){
+		System.out.print("\n");
+		int i = 0;
+		while (i < 40)
+		{
+			System.out.pring("-");
+			i++;
+		}
+		System.out.print("\n");
+		i = 0;
+		while (i < clkCrt.size())
+		{
+			System.out.print(i + "\t|\t" +clkCtr.get(i_[0] + "\t|\t" + clkCtr.get(i)[1] + "\n");
+			i++;
+		}
+		i = 0;
+		while (i < 40)
+		{
+			System.out.print("-");
+			i++;
+		}
+		System.out.print("\n");
+	}
+
+	public static int swpOut(int ppn){
+		int swpPg = 0;
+		if(swpAd.size() == 0){
+			swpPg = currSwpPg;
+			currSwpPg++;
+		}
+		else{
+			swpPg = SwpAd.pop();
+		}
+
+		byte[] memory = Machine.processor().getMemory();
+		int written = swp.write(swpPg * Processor.pageSize, memory, ppn * Processor.pageSize, Processor.pageSize);
+		return swpPg;
+	}
+
+	public static byte[] swpIn(int swpPg){
+		byte[] content = new byte[Processor.pageSize];
+		swp.read(swpPg*Processor.pageSize, content, 0, Processor.pageSize);
+		return content;
+	}
+
+	public static int physPageNumber(int vpn, VMProcess process){
+		int ppn = physPagesAvailable.pop();
+		clkCtr.set(ppn, new Integer[]{vpn, 1});
+		processes.set(ppn, process);
+		return ppn;
+	}
+
 
 	// dummy variables to make javac smarter
 	private static VMProcess dummy1 = null;
 
-	public static Lock vmLock;
-
 	private static final char dbgVM = 'v';
 
-	// * Helper Methods !! *//
+	private static int pageSize;
 
-	/*
-	 * Finds next available physical page, links it to calling process, and then
-	 * returns new ppn
-	 */
-	public static int physPageNumber(VMProcess process, int vpn) {
-		int ppn = physPagesAvailable.pop();
-		processes.set(ppn, process);
-		return  ppn;
-	}
+	private static ArrayList<Integer[]> clkCtr = new ArrayList<Integer[]>();
 
-	public static int pageReplacement(VMProcess pricess, int vpn){
-		return -1;	
-	}
+	private static ArrayList<VMProcess> processes = new ArrayList<VMProcess>();
 
-	// Kernel maintains list of processes
-	private static ArrayListt<VMProcess> processes = new ArrayList<VMProcess>();
+	private static int clkIdx;
+
+	private static OpenFile swp;
+
+	public static LinkedList<Integer> swpAd = new LinkedList<Integer>();
+
+	private static int currSwpPg = 0;
 }
