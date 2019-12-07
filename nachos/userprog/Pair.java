46f21c8c8927c55c178d498dec97e872c33ea021
sssaiya <sssaiya@ucsd.edu>
2019-12-06 20:10:54 -0800

    Debug

---
+++ nachos/userprog/Pair.java
@@ -0,0 +1,22 @@
+
+public class Pair<K, V> {
+    private final K a;
+    private final V b;
+
+    public static <K, V> Pair<K, V> createPair(K a, V b) {
+        return new Pair<K, V>(a, b);
+    }
+
+    public Pair(K A, V B) {
+        this.a = A;
+        this.b = B;
+    }
+
+    public K getKey() {
+        return a;
+    }
+
+    public V getValue() {
+        return b;
+    }
+}
--- nachos/userprog/UserProcess.java
+++ nachos/userprog/UserProcess.java
@@ -1012,25 +1012,3 @@ public class UserProcess {
 
 	private static final char dbgProcess = 'a';
 }
-
-public class Pair<K, V> {
-	private final K a;
-	private final V b;
-
-	public static <K, V> Pair<K, V> createPair(K a, V b) {
-		return new Pair<K, V>(a, b);
-	}
-
-	public Pair(K A, V B) {
-		this.a = A;
-		this.b = B;
-	}
-
-	public K getKey() {
-		return a;
-	}
-
-	public V getValue() {
-		return b;
-	}
-}
